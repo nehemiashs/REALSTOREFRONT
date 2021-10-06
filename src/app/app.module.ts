c569021fdf40d1a526ce9bfe8c4c407cd44e37ea
DarkCode <test@fake.com>
2021-10-05 20:58:07 -0500

    make it better

---
+++ src/app/app.module.ts
@@ -0,0 +1,59 @@
+import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+import { ChartsModule } from 'ng2-charts';
+import { NgxPayPalModule } from 'ngx-paypal';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { AdministracionComponent } from './componentes/administracion/administracion.component';
+import { AutenticationComponent } from './componentes/autentication/autentication.component';
+import { CarritoComprasComponent } from './componentes/carrito-compras/carrito-compras.component';
+import { CatalogoProductoComponent } from './componentes/catalogo-producto/catalogo-producto.component';
+import { DetalleProductoComponent } from './componentes/detalle-producto/detalle-producto.component';
+import { HistorialComponent } from './componentes/historial/historial.component';
+import { HOMEComponent } from './componentes/home/home.component';
+import { InformesComponent } from './componentes/informes/informes.component';
+import { MapsComponent } from './componentes/maps/maps.component';
+import { PerfilComponent } from './componentes/perfil/perfil.component';
+import { ProductosComponent } from './componentes/productos/productos.component';
+import { RegistroUsuarioComponent } from './componentes/registro-usuario/registro-usuario.component';
+import { UsuariosComponent } from './componentes/usuarios/usuarios.component';
+import { FilterPipe } from './pipes/filter.pipe';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HOMEComponent,
+    DetalleProductoComponent,
+    CatalogoProductoComponent,
+    CarritoComprasComponent,
+    AutenticationComponent,
+    AdministracionComponent,
+    PerfilComponent,
+    HistorialComponent,
+    UsuariosComponent,
+    ProductosComponent,
+    FilterPipe,
+    InformesComponent,
+    MapsComponent,
+    RegistroUsuarioComponent
+  ],
+  imports: [
+    ReactiveFormsModule,
+    NgxPayPalModule,
+    /* MdbCheckboxModule, */
+    CommonModule,
+    FormsModule,
+    HttpClientModule,
+    BrowserModule,
+    AppRoutingModule,
+    /* NgbModule */
+    ChartsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
